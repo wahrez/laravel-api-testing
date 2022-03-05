0cef1a854a39929e15e4909044892b98d20f2b7f
Mustafa Wahrez <mustafawahrez@gmail.com>
2022-03-05 19:24:25 +0430

    write login rout test

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -68,4 +69,47 @@ class AuthenticationTest extends TestCase
                 "message"
             ]);
     }
+
+    public function testMustEnterEmailAndPassword()
+    {
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "The given data was invalid.",
+                "errors" => [
+                    'email' => ["The email field is required."],
+                    'password' => ["The password field is required."],
+                ]
+            ]);
+    }
+
+    public function testSuccessfulLogin()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'sample@test.com',
+            'password' => bcrypt('sample123')
+        ]);
+
+        $loginData = [
+            'email' => 'sample@test.com',
+            'password' => 'sample123'
+        ];
+
+        $this->json('POST', 'api/login', $loginData, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "user" => [
+                    'id',
+                    'name',
+                    'email',
+                    'email_verified_at',
+                    'created_at',
+                    'updated_at',
+                ],
+                "access_token",
+                "message"
+            ]);
+        $this->assertAuthenticated();
+    }
+
 }
